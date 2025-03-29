f82aa4829a245ea06abcdc3a3c3bb539022d8d59
jkky-98 <aal2525@ajou.ac.kr>
2025-03-29 21:00:51 +0900

    fix: dto받도록 수정

--- task/src/main/java/com/antock/task/controller/TeleSalesInfoImportController.java
+++ task/src/main/java/com/antock/task/controller/TeleSalesInfoImportController.java
@@ -1,8 +1,10 @@
 package com.antock.task.controller;
 
+import com.antock.task.controller.dto.TeleSalesSaveRequest;
 import com.antock.task.domain.TeleSalesInfo;
 import com.antock.task.service.TeleSalesInfoService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,15 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class TeleSalesInfoImportController {
 
     private final TeleSalesInfoService teleSalesInfoService;
 
     @PostMapping("/save")
     public ResponseEntity<?> saveTeleSalesInfo(
-            @RequestBody TeleSalesInfo teleSalesInfo
+            @RequestBody TeleSalesSaveRequest request
     ) {
-        teleSalesInfoService.save(teleSalesInfo);
+        log.info("request : {}", request);
+        teleSalesInfoService.save(request);
         return ResponseEntity.ok().build();
     }
 }
--- task/src/main/java/com/antock/task/service/TeleSalesInfoService.java
+++ task/src/main/java/com/antock/task/service/TeleSalesInfoService.java
@@ -1,16 +1,25 @@
 package com.antock.task.service;
 
+import com.antock.task.controller.dto.TeleSalesSaveRequest;
 import com.antock.task.domain.TeleSalesInfo;
+import com.antock.task.service.externalapi.csv.CsvDownloadRequest;
+import com.antock.task.service.externalapi.csv.CsvParser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class TeleSalesInfoService {
 
+    private final CsvParser csvParser;
+
     @Transactional
-    public void save(TeleSalesInfo teleSalesInfo) {
+    public void save(TeleSalesSaveRequest request) {
+        List<CsvDownloadRequest> csvData = csvParser.parse(request);
+
 
     }
 }
